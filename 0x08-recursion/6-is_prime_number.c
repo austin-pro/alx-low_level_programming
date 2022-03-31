20a3f4cc1342ee9c525b5b0e78e599f90ebb8c55
austin-pro <austinhongo@gmail.com>
2022-03-31 14:05:31 -0700

    recursions flow

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * check - calculates if a number is prime
+ *
+ * @n: number to evaluate
+ *
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ *
+ **/
+
+int check(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (check(n, i - 1));
+}
+
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ *
+ * @n: number to evaluate
+ *
+ *
+ * Return:resulting prime number
+ *
+ **/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check(n, n - 1));
+}
+
