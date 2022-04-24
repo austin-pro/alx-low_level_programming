9b2a001445cac0a8c015eb84ebdc1da4f98819cd
austin-pro <austinhongo@gmail.com>
2022-04-24 10:35:32 -0700

    Variadic fuctions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(list, char *);
-		
 		if (!str)
 			str = "(nil)";
 		if (!separator)
